56b68e10c05b6394e55972c5cd0066827f7b949b
Sergiy Lanoviy <lanovyi.s@gmail.com>
2016-10-05 18:02:47 +0300

    7

--- js7/js/script.js
+++ js7/js/script.js
@@ -1,4 +1,3 @@
-
 //-----------------------JS-7------------------/
 $(function () {
     var $liElem = $('li');
@@ -12,28 +11,31 @@ $(function () {
     var $liElemThird = $('.li_third');
 
 
-$liElem.on('click', function (e) {
-    e.preventDefault();
-    $liElem.removeClass('active_li');
-    $(this).addClass('active_li');
-    var $aActive = $(this).find('a');
-    $aElem.removeClass('active_a');
-    $aActive.addClass('active_a');
-    $pElem.css('display', 'none');
-});
-
-
-    $liElemFirst.on('click', function () {
-        $pElemFirst.slideToggle();
+    $liElem.on('click', function (e) {
+        e.preventDefault();
+        $liElem.removeClass('active_li');
+        $(this).addClass('active_li');
+        $pElem.css('display', 'none');
+        $(this).index();
+        console.log($(this).index());
+        $pElem.index();
+        index = $(this).index();
+        console.log($pElem.index());
+        $pElem.slideToggle();
     });
 
-    $liElemSecond.on('click', function () {
-        $pElemSecond.slideToggle();
-    });
 
-    $liElemThird.on('click', function () {
-        $pElemThird.slideToggle();
-    });
+    // $liElemFirst.on('click', function () {
+    //     $pElemFirst.slideToggle();
+    // });
+    //
+    // $liElemSecond.on('click', function () {
+    //     $pElemSecond.slideToggle();
+    // });
+    //
+    // $liElemThird.on('click', function () {
+    //     $pElemThird.slideToggle();
+    // });
 
 
 });
