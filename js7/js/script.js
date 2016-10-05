8a66a389cd96861d16f37f1703ee0d05993936b8
Sergiy Lanoviy <lanovyi.s@gmail.com>
2016-10-05 14:13:16 +0300

    js-7

--- js7/js/script.js
+++ js7/js/script.js
@@ -1,56 +1,50 @@
-console.log($);
+
 //-----------------------JS-7------------------/
 $(function () {
     var $liElem = $('li');
     var $pElem = $('p');
     var $aElem = $('a');
-    var $aFirst = $('.li_first a');
-    var $aSecond = $('.li_second a');
-    var $aThird = $('.li_third a');
+    // var $aFirst = $('.li_first a');
+    // var $aSecond = $('.li_second a');
+    // var $aThird = $('.li_third a');
     var $pElemFirst = $('.first');
     var $pElemSecond = $('.second');
     var $pElemThird = $('.third');
-    var $liElemFirst = $('.li_first');
+    var $liElemFirst = $('.active_li');
     var $liElemSecond = $('.li_second');
     var $liElemThird = $('.li_third');
+    var $mainBlock = $('div.main-block');
+
+    console.log($mainBlock);
+
+$liElem.on('click', function (e) {
+    e.preventDefault();
+    $liElem.removeClass('active_li');
+    $(this).addClass('active_li');
+    var $aActive = $(this).find('a');
+    $aElem.removeClass('active_a');
+    $aActive.addClass('active_a');
+
+
+});
+
 
     $liElemFirst.on('click', function (e) {
         e.preventDefault();
-        console.log('onclick');
-        $liElem.css('background', 'white');
-        $(this).css('background', 'blue');
-        $(this).css('color', 'white');
         $pElem.css('display', 'none');
         $pElemFirst.slideToggle();
-        $aElem.css('color','black');
-        $aFirst.css('color','white');
     });
 
     $liElemSecond.on('click', function (e) {
         e.preventDefault();
-        console.log('onclick');
-        $liElem.css('background', 'white');
-        $(this).css('background', 'blue');
         $pElem.css('display', 'none');
         $pElemSecond.slideToggle();
-        $aElem.css('color','black');
-        $aSecond.css('color','white');
     });
 
     $liElemThird.on('click', function (e) {
         e.preventDefault();
-        console.log('onclick');
-        $liElem.css('background', 'white');
-        $(this).css('background', 'blue');
         $pElem.css('display', 'none');
         $pElemThird.slideToggle();
-        $aElem.css('color','black');
-        $aThird.css('color','white');
     });
-
-    //-----------------------------JS-8----------------------/
-
-
-
 
 });
