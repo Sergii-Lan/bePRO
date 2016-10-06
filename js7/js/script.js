0f10379bafe50d694ba2057c8a59607799606b18
Sergiy Lanoviy <lanovyi.s@gmail.com>
2016-10-06 12:23:08 +0300

    7

--- js7/js/script.js
+++ js7/js/script.js
@@ -1,41 +1,62 @@
 //-----------------------JS-7------------------/
-$(function () {
-    var $liElem = $('li');
-    var $pElem = $('p');
-    var $aElem = $('a');
-    var $pElemFirst = $('.first');
-    var $pElemSecond = $('.second');
-    var $pElemThird = $('.third');
-    var $liElemFirst = $('.active_li');
-    var $liElemSecond = $('.li_second');
-    var $liElemThird = $('.li_third');
+$(document).ready(function () {
+    $(function () {
+        var $liElem = $('li');
+        var $pElem = $('p');
+        var $aElem = $('a');
+        var $pElemFirst = $('.first');
+        var $pElemSecond = $('.second');
+        var $pElemThird = $('.third');
+        var $liElemFirst = $('.active_li');
+        var $liElemSecond = $('.li_second');
+        var $liElemThird = $('.li_third');
 
 
-    $liElem.on('click', function (e) {
-        e.preventDefault();
-        $liElem.removeClass('active_li');
-        $(this).addClass('active_li');
-        $pElem.css('display', 'none');
-        $(this).index();
-        console.log($(this).index());
-        $pElem.index();
-        index = $(this).index();
-        console.log($pElem.index());
-        $pElem.slideToggle();
+        $liElem.on('click', function (e) {
+            e.preventDefault();
+            $liElem.removeClass('active_li');
+            $(this).addClass('active_li');
+            $pElem.css('display', 'none');
+
+            // index = $(this).index();
+
+            // switch (index) {
+            //     case 0: {
+            //         $pElemFirst.slideToggle();
+            //         break;
+            //     }
+            //     case 1: {
+            //         $pElemSecond.slideToggle();
+            //         break;
+            //     }
+            //     case 2: {
+            //         $pElemThird.slideToggle();
+            //         break;
+            //     }
+            // }
+
+
+            $(this).index();
+            console.log($(this).index());
+            index = $(this).index(this);
+            console.log($pElem.index(index));
+            $('.main_block').find('p').index().slideToggle();
+            // $pElem.slideToggle();
+        });
+
+
+        // $liElemFirst.on('click', function () {
+        //     $pElemFirst.slideToggle();
+        // });
+        //
+        // $liElemSecond.on('click', function () {
+        //     $pElemSecond.slideToggle();
+        // });
+        //
+        // $liElemThird.on('click', function () {
+        //     $pElemThird.slideToggle();
+        // });
+
+
     });
-
-
-    // $liElemFirst.on('click', function () {
-    //     $pElemFirst.slideToggle();
-    // });
-    //
-    // $liElemSecond.on('click', function () {
-    //     $pElemSecond.slideToggle();
-    // });
-    //
-    // $liElemThird.on('click', function () {
-    //     $pElemThird.slideToggle();
-    // });
-
-
 });
